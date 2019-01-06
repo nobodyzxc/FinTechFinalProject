d2a4e6a9ac0ab687d57b6e57ca59a3d210d3f354
nobodyzxc <dici86.chen@gmail.com>
2019-01-07 06:03:18 +0800

    add two shop

--- database/new.js
+++ database/new.js
@@ -32,6 +32,8 @@ function createNewDatabase(){
   l_sqlstr = "CREATE TABLE heat (r_name char, people int, total int);"
   l_sqlstr += "INSERT INTO heat VALUES ('樂山食堂', 25, 60);"
   l_sqlstr += "INSERT INTO heat VALUES ('四五大街', 55, 80);"
+  l_sqlstr += "INSERT INTO heat VALUES ('湯饌', 33, 40);"
+  l_sqlstr += "INSERT INTO heat VALUES ('永康街左撇子', 25, 40);"
   l_sqlstr += "INSERT INTO heat VALUES ('金鮨日式料理', 15, 30);"
   db.run(l_sqlstr);
 
@@ -39,7 +41,8 @@ function createNewDatabase(){
   l_sqlstr = "CREATE TABLE location (r_name char, longitude float, latitude float);"
   l_sqlstr += "INSERT INTO location VALUES ('樂山食堂', 24.988998, 121.573640);"
   l_sqlstr += "INSERT INTO location VALUES ('四五大街', 24.988303, 121.574231);"
-
+  l_sqlstr += "INSERT INTO location VALUES ('湯饌', 24.9876791,121.5751377);"
+  l_sqlstr += "INSERT INTO location VALUES ('永康街左撇子', 24.9878874, 121.5766012);"
   l_sqlstr += "INSERT INTO location VALUES ('金鮨日式料理', 24.987119, 121.578585);"
 
   db.run(l_sqlstr);
@@ -57,6 +60,12 @@ function createNewDatabase(){
   m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('四五大街', '叉燒煎蛋飯', 80);"
   m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('四五大街', '奶油雞排飯', 85);"
   m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('四五大街', '滑蛋雞絲飯', 75);"
+  m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('永康街左撇子', '牛肉燴飯', 80);"
+  m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('永康街左撇子', '蝦仁炒飯', 80);"
+  m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('永康街左撇子', '宮保雞丁飯', 100);"
+  m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('湯饌', '滑蛋唐揚雞丼飯', 110);"
+  m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('湯饌', '黑鐵炙燒豚', 95);"
+  m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('湯饌', '海鮮鍋', 120);"
   m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('金鮨日式料理', '親子丼', 90);"
   m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('金鮨日式料理', '鮮蝦手捲', 40);"
   m_sqlstr += "INSERT INTO dish(r_name, d_name, price) VALUES ('金鮨日式料理', '政妹蓋飯', 150);"
